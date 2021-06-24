155b2142c96beb616c185f0582ecff90bf09d2fe
guangtao.wang <guangtao.wang@jd.com>
2021-06-24 14:53:14 -0700

    hotpotqa_script

--- hotpotqa_run.sh
+++ hotpotqa_run.sh
@@ -52,19 +52,19 @@ preprocess() {
         # Output: para_ir_combined.json
         python longformer_retrieval/0_lb_longformer_dataprepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/para_ir_combined.json
 
-        echo "2. Paragraph ranking (2): longformer retrieval ranking scores"
-        # switch to Longformer for final leaderboard, PYTORCH LIGHTING + '1.0.8' TRANSFORMER (3.3.1)
-        # Output: long_para_ranking.json
-        python longformer_retrieval/0_lb_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/HotPotQA_PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
-
-        echo "3. MultiHop Paragraph Selection (3)"
-        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
-        # Output: long_multihop_para.json
-        python longformer_retrieval/0_lb_longformer_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/long_para_ranking.json $OUTPUT_PROCESSED/long_multihop_para.json $SELECTEED_DOC_NUM
-
-        echo "4. Dump features for electra do_lower_case"
-        # Input: $INPUT_FILE, long_multihop_para.json; model_type, model_name, doc_link_ner.json, ner.json
-        python sd_mhqa/hotpotqa_dump_features.py  --para_path $OUTPUT_PROCESSED/long_multihop_para.json --full_data $INPUT_FILE --model_name_or_path $ELECTRA_ROOT/electra-base-discriminator --do_lower_case --ner_path $OUTPUT_PROCESSED/ner.json --model_type electra --tokenizer_name $ELECTRA_ROOT/electra-base-discriminator --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json --ranker long --data_type $DATA_TYPE --max_para_num $SELECTEED_DOC_NUM
+#        echo "2. Paragraph ranking (2): longformer retrieval ranking scores"
+#        # switch to Longformer for final leaderboard, PYTORCH LIGHTING + '1.0.8' TRANSFORMER (3.3.1)
+#        # Output: long_para_ranking.json
+#        python longformer_retrieval/0_lb_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/HotPotQA_PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
+#
+#        echo "3. MultiHop Paragraph Selection (3)"
+#        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
+#        # Output: long_multihop_para.json
+#        python longformer_retrieval/0_lb_longformer_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/long_para_ranking.json $OUTPUT_PROCESSED/long_multihop_para.json $SELECTEED_DOC_NUM
+#
+#        echo "4. Dump features for electra do_lower_case"
+#        # Input: $INPUT_FILE, long_multihop_para.json; model_type, model_name, doc_link_ner.json, ner.json
+#        python sd_mhqa/hotpotqa_dump_features.py  --para_path $OUTPUT_PROCESSED/long_multihop_para.json --full_data $INPUT_FILE --model_name_or_path $ELECTRA_ROOT/electra-base-discriminator --do_lower_case --ner_path $OUTPUT_PROCESSED/ner.json --model_type electra --tokenizer_name $ELECTRA_ROOT/electra-base-discriminator --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json --ranker long --data_type $DATA_TYPE --max_para_num $SELECTEED_DOC_NUM
 
     done
 
