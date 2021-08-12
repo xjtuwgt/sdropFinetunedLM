e7aa5f0b78f8a9c4ea5161f1d62403a2898f9cb9
Peng Qi <qipeng@users.noreply.github.com>
2021-08-11 17:14:11 -0700

    Add docred download script

---
+++ dataset/docred/download_docred_data.sh
@@ -0,0 +1,5 @@
+gdown https://drive.google.com/uc?id=1fDmfUUo5G7gfaoqWWvK81u08m71TK2g7 -O dev.json
+gdown https://drive.google.com/uc?id=1y9A0zKrvETc1ddUFuFhBg3Xfr7FEL4dW -O rel_info.json
+gdown https://drive.google.com/uc?id=1lAVDcD94Sigx7gR3jTfStI66o86cflum -O test.json
+gdown https://drive.google.com/uc?id=1NN33RzyETbanw4Dg2sRrhckhWpzuBQS9 -O train_annotated.json
+gdown https://drive.google.com/uc?id=1Qr4Jct2IJ9BVI86_mCk_Pz0J32ww9dYw -O train_distant.json
